e28a1ab55fbb7922d3e61e284f27c514dd2f1f58
=Yusuf <=yusuf.nezir07@gmail.com>
2026-08-26 14:17:34 +0200

    sign in page task by teacher

--- src/componentes/SignIn.jsx
+++ src/componentes/SignIn.jsx
@@ -5,11 +5,11 @@ export default function SignIn(){
             <p class="text-5xl font-extrabold tracking-tight text-red-700 sm:text-5xl md:text-6xl">
                 Im
             </p>
-            <input type="text" placeholder="Dit navn" class="text-5xl"></input>
+            <input type="text" placeholder="Dih navn" class="text-5xl"></input>
             <p class="text-5xl font-extrabold tracking-tight text-red-700 sm:text-5xl md:text-6xl">
                 MY PASSWORD IS
             </p>
-            <input type="text" placeholder="Din adgangskode" class="text-5xl text-red-700"></input>
+            <input type="text" placeholder="D1h 4dg4ng5k0d3" class="text-5xl text-red-700"></input>
         </div>
         </>
     )
--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -6,8 +6,8 @@ export default function Header() {
             <h1 class="text-5xl font-extrabold tracking-tight text-red-700 sm:text-5xl md:text-6xl">Hej</h1>
         </div>
         <div class="flex flex-row items-center justify-center flex-wrap">
-        <img src={heroImg} />
-        <img src={heroImg}/>
+            <img src={heroImg} />
+            <img src={heroImg}/>
         </div>
         </>
     )
