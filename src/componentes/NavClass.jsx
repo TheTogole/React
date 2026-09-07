7da1426846a51ae288ea172405b79f3d031ef250
TheTogole <Yusuf.nezir07@gmail.com>
2026-09-07 13:50:39 +0200

    Made Leaderboard anchor to a button (Teacher: "this is amazing" Student: "I want to be happier")

--- src/componentes/NavClass.jsx
+++ src/componentes/NavClass.jsx
@@ -1,12 +1,17 @@
 export default function NavClass() {
   return (
     <nav className="nav">
-      <a className=" site-title flex items-center justify-center text-1xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl mx-4" href="/" >
+      <a className=" site-title flex items-center justify-center text-4x1 font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl mx-4" href="/" >
         Overview
       </a>
       <ul>
         <li className="flex items-center justify-center text-1x1 font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl mx-4">
-          <a className="mr-4" href="/LeaderBoard">LeaderBoard</a>
+          <a className="mr-4" href="/LeaderBoard">
+          <button class="bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 ...">
+  LeaderBoard
+</button>
+          </a>
+
           <a className="mr-4" href="/StatsPage">StatsPage</a>
           <a className="mr-4" href="/Sign-In">Sign-In</a>
         </li>
